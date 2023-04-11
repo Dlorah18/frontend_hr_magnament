ce292a0bb0c8cef23bf337efb67505776b72c851
Dlorah18 <124593707+Dlorah18@users.noreply.github.com>
2023-04-11 13:26:24 -0500

    Creacion de las vistas de la tabla de listar usuarios y el formulario de usuarios

---
+++ src/views/User/ListarUsuariosView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from '../../components/HeaderComponent.vue';
+</script>
+<template>
+    <HeaderComponent></HeaderComponent>
+    <div  class="container-secsion">
+        <div>
+            <table>
+            <thead>
+                <tr>
+                    <th>Codigo</th>
+                    <th>Nombres</th>
+                    <th>Apellidos</th>
+                    <th>Rol</th>
+                    <th>Estado</th>
+                    <th>Fecha Creacion</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Codigo</td>
+                    <td>Nombres</td>
+                    <td>Apellidos</td>
+                    <td>Rol</td>
+                    <td>Estado</td>
+                    <td>Fecha Creacion</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+        
+    </div>
+</template>
+<script>
+</script>
