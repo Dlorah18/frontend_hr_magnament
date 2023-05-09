c13a5c6e8e0d6216a22f6f42f134585ad32a29e8
unknown <Harold@DESKTOP-DF9MMK4.com>
2023-05-09 12:58:15 -0500

    Componentes modal y tabla usuario con los iconos

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -5,24 +5,20 @@
         <div class="modal-wrapper">
           <div class="modal-container">
 
-            <div class="modal-header">
-              <slot name="header">
+            <div class="modal-header row">
                 <img :src="'/images/' + img"/>
-              </slot>
             </div>
 
-            <div class="modal-body">
-              <slot name="body">
+            <div class="modal-body row">
+              <div name="body" class="col-lg-12">
                 {{ msg }}
-              </slot>
+              </div>
+              <div class="col-lg-12">
+                <input class="btn btn-secondary btn-sm" @click="$emit('close')" type="text" value="Cerrar">
+            </div>
             </div>
 
-            <div class="form-btn">
-                <button class="login100-form-btn" @click="$emit('close')">
-                  <p>Cerrar</p>
-                </button>
-
-            </div>
+            
           </div>
         </div>
       </div>
--- src/components/User/TableComponent.vue
+++ src/components/User/TableComponent.vue
@@ -1,77 +1,64 @@
 <template>
-    <div>
-        <div class="container-table">
-            <v-table>
-
-            </v-table>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Codigo</th>
-                        <th>Nombres</th>
-                        <th>Apellidos</th>
-                        <th>Rol</th>
-                        <th>Estado</th>
-                        <th>Fecha Creacion</th>
-                        <th>Accion</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="registro in data">
-                        <td>{{ registro.id }}</td>
-                        <td>{{ registro.nombres }}</td>
-                        <td>{{ registro.apellidos }}</td>
-                        <td>{{ registro.nomRol }}</td>
-                        <td v-bind:class="{
-                            'activate-state': registro.estado === 'Activo',
-                            'inactivate-state': registro.estado === 'Inactivo'
-                        }">
-                            {{ registro.estado }}
-                        </td>
-                        <td>{{ registro.fechaCreacion }}</td>
-                        <td>Accion</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div>
-            <paginate v-model="currentPage" :page-count="pageCount" :margin-pages="2" :prev-text="'Anterior'"
-                :next-text="'Siguiente'" :container-class="'pagination'" :page-class="'page-item'" :active-class="'active'"
-                :disabled-class="'disabled'"></paginate>
-        </div>
+  <div class="row">
+    <div class="container-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Codigo</th>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+            <th>Rol</th>
+            <th>Estado</th>
+            <th>Fecha Creacion</th>
+            <th>Accion</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="registro in data">
+            <td>{{ registro.id }}</td>
+            <td>{{ registro.nombres }}</td>
+            <td>{{ registro.apellidos }}</td>
+            <td>{{ registro.nomRol }}</td>
+            <td v-bind:class="{
+              'activate-state': registro.estado === 'Activo',
+              'inactivate-state': registro.estado === 'Inactivo',
+            }">
+              {{ registro.estado }}
+            </td>
+            <td>{{ registro.fechaCreacion }}</td>
+            <td>
+              <div class="row">
+                <div class="col-6">
+                  <img  class="icon" src="../../../public/images/lupa.png">
+                </div>
+                <div class="col-6">
+                  <img class="icon" src="../../../public/images/editar.png">
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 <script>
-import Paginate from 'vuejs-paginate'
-
+import { listUsers } from "../../services/AdministradorServices";
 export default {
-    components:{
-        Paginate
+  data() {
+    return {
+      data: null
+    };
+  },
+  methods: {
+    listUsers() {
+      const response = listUsers().then((res) => {
+        this.data = res.data;
+      });
     },
-    props: {
-        data: Array
-    },
-    data() {
-        return {
-            itemsPerPage: 10,
-            currentPage: 1
-        }
-    }, methods: {
-        paginatedItems() {
-            const startIndex = (this.currentPage - 1) * this.itemsPerPage
-            const endIndex = startIndex + this.itemsPerPage
-            return this.data.slice(startIndex, endIndex)
-        },
-        pageCount() {
-            return Math.ceil(this.data.length / this.itemsPerPage)
-        }
-    },
-    created() {
-        console.log(this.data)
-    },
-    mounted() {
-
-    }
-}
-
+  },
+  created() {
+    this.listUsers()
+  }
+};
 </script>
