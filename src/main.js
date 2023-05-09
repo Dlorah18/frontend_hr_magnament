4b199fb45f73831306fbdf65745ad4ffa8b01a13
unknown <Harold@DESKTOP-DF9MMK4.com>
2023-05-09 13:00:30 -0500

    bootstrap

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios  from 'vue-axios'
-import VuePaginate from 'vuejs-paginate'
 import './assets/main.css'
+
+
+
 const app = createApp(App)
 
 app.use(router)
-app.component('paginate', VuePaginate)
 app.use(VueAxios ,axios)
 app.mount('#app')
 
